dc571242fc671acf937a9cc92e00c756cf51f616
Aadityadav <aadit.211602@ncit.edu.np>
2025-01-14 23:45:11 +0545

    changing kills cards

--- src/Components/Cards/SkillsCards/SkillsCards.js
+++ src/Components/Cards/SkillsCards/SkillsCards.js
@@ -1,23 +1,39 @@
-import React from 'react'
-import './SkillsCards.css';
+// import React from 'react'
+// import './SkillsCards.css';
+// export default function SkillsCards(props) {
+//   return (
+//     <>
+
+//   <div className="Scard">
+//     <div className="Scard-inner">
+//       <div className="Scard-front">
+//         <h3>{props.title}</h3>
+//         <img src={props.logo} alt='fuck man' style={{ width: "100px", height: "auto" }}/>
+//       </div>
+//       <div className="Scard-back">
+//         <p>{props.description}</p>
+//       </div>
+//     </div>
+//   </div>
+
+//     </>
+//   )
+// }
+
+import React from "react";
+import "./SkillsCards.css";
 export default function SkillsCards(props) {
   return (
     <>
-  
-  <div className="Scard">
-    <div className="Scard-inner">
-      <div className="Scard-front">
-        <h3>{props.title}</h3>
-        <img src={props.logo} alt='fuck man' style={{ width: "100px", height: "auto" }}/>
+      <div className="Scard">
+        <div className="Scard-image-container">
+        <img src={props.logo} style={{ width: "100px", height: "auto" }}/>
+        </div>
+        <p className="Scard-title">{props.title}</p>
+        <p className="Scard-des">
+        {props.description}
+        </p>
       </div>
-      <div className="Scard-back">
-        <p>{props.description}</p>
-      </div>
-    </div>
-  </div>
-
-
-
     </>
-  )
+  );
 }
--- src/Pages/Skills/Skills.js
+++ src/Pages/Skills/Skills.js
@@ -14,31 +14,31 @@ const skillsData = [
   {
     title: "ReactJS Development",
     logo:Reactjs,
-    description: "Skilled in building responsive web apps with React.js, using components, hooks, and state management to create dynamic and user-friendly interfaces.",
+    description: "Skilled in building responsive web apps with React.js, using components, hooks, and state management.",
   },
 
   {
     title: "NextJS  Development",
     logo:nextjs,
-    description: "Experienced in building fast, SEO-friendly web applications with Next.js, leveraging server-side rendering, dynamic routing, and API integration."
+    description: "Experienced in building fast, SEO-friendly web applications, leveraging server-side rendering, dynamic routing ."
+  },
+
+  {
+    title: "Python(Django)",
+    logo:python,
+    description: "Building applications using Django, with expertise in RESTful APIs, database integration, and backend optimization."
   },
 
   {
     title: "UI/UX Design",
     logo:UIUX,
-    description: "Proficient in crafting visually appealing interfaces, focusing on user-centric design and seamless experiences across web and mobile platforms."
+    description: "Proficient in crafting visually appealing interfaces, focusing on user-centric design and seamless experiences."
   },
 
   {
     title: "WordPress Development",
     logo:wordpress,
-    description: "Experienced in creating and customizing WordPress websites, optimizing themes, plugins, and performance for diverse business needs."
-  },
-
-  {
-    title: "Python(Django) Development",
-    logo:python,
-    description: "Skilled in building  scalable web applications using Django, with expertise in RESTful APIs, database integration, and backend optimization."
+    description: "Experienced in creating and customizing WordPress websites, optimizing themes, plugins, and performance."
   }
   
 ];
@@ -56,29 +56,3 @@ export default function Skills() {
   )
 }
    
-
-// import React from 'react'
-// import './Skills.css';
-// import SkillsCards from './../../Components/Cards/SkillsCards/SkillsCards';
-
-
-// export default function Skills() {
-//   return (
-//     <>
-
-  
-
-//   <div className='SkillsPage'>
-//     <h1  className='skilltitle'> <u> Skills </u> </h1>
-    
-//     <div className='skillsCards'>
-//        <SkillsCards title={"React Development"} description={" If you have a variable to send, and not a string as in the example above, you just put the variable name inside curly brackets"} link={"https://aadityadav.com.np/"} />
-//        <SkillsCards title={"UI/UX Design"} description={"Props are also how you pass data from one component to another, as parameters. "}/>
-//        <SkillsCards title={"WordPress Development"} description={" If you have a variable to send, and not a string as in the example above, you just put the variable name inside curly brackets"}/>
-//        <SkillsCards title={"Python Development"} description={" If you have a variable to send, and not a string as in the example above, you just put the variable name inside curly brackets"}/>
-//        <SkillsCards title={"Django Development"} description={" If you have a variable to send, and not a string as in the example above, you just put the variable name inside curly brackets"}/>
-//     </div>
-       
-//     </div>
-//   </>
-//   )}
