8d39211c15dd4d128b6d3b8a7943ef69ebfeb795
Aadityadav <aadit.211602@ncit.edu.np>
2025-02-01 21:36:34 +0545

    adding cv to download btn

--- src/Pages/background/background.js
+++ src/Pages/background/background.js
@@ -1,13 +1,13 @@
 import React from "react";
 import "./background.css";
-import Album from "../../Components/Album/Album";
+// import Album from "../../Components/Album/Album";
 
 
 export default function Background() {
   return (
     <div className="background">
 
-      <Album/>
+      {/* <Album/> */}
 
     </div>
   );
