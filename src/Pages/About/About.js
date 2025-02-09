89d4d01e90e020a2c05fad21d780c285156ad08f
Aadityadav <aadit.211602@ncit.edu.np>
2025-02-09 14:53:10 +0545

    monor changes in my project

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -41,7 +41,7 @@ export default function About() {
             <blockquote className="Atext">
               I'm <u>Aadit Yadav </u>, a passionate software engineering student
               with a keen interest in web development (Backend, Frontend), data
-              science and machine learning. . Currently, I'm honing my skills in
+              science and machine learning. Currently, I'm honing my skills in
               Python development, building intuitive web applications using
               React, and exploring the world of AI and data-driven technologies.
               <br />
