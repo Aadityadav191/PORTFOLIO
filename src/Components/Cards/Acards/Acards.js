f6b401e30f10937272f6c5ece2d8947e3bfcdb38
Aadit yadav <yaaditya191@gmail.com>
2026-06-02 12:33:24 +0545

    Style: changing the css and appearence of cards

--- src/Components/Cards/Acards/Acards.js
+++ src/Components/Cards/Acards/Acards.js
@@ -6,13 +6,13 @@ export default function Acards(props) {
   return (
     <>
       <motion.div
-        className="parent"
+        className="Aparent"
         initial={{ opacity: 0, y: 50 }} 
         whileInView={{ opacity: 1, y: 0 }} 
         transition={{ duration:0.5, delay: 0.75}} 
         viewport={{ once: true }}
       >
-        <div className="card">
+        <div className="Acard">
           <div className="content-box">
             <span className="card-title">{props.Title}</span>
             <p className="card-content">
